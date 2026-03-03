9e99d9ad75ae3821eb1397e8cff95579dc504998
carloslopez1234-ya <1234.carloslopezzzz@gmail.com>
2026-03-03 18:53:57 +0000

    I did this

---
+++ Car.java
@@ -0,0 +1,61 @@
+// Asssmesment 
+
+
+
+public class Car {
+    
+         
+
+        private String car_ID; 
+        private String Brand;
+        private String model;
+        private int year;
+        private String Fuel_type;
+        private String Color;
+        private double Mileage_kmpl;
+        public String getCar_ID() {
+            return car_ID;
+        }
+
+        public void setCar_ID(String car_ID) {
+            this.car_ID = car_ID;
+        }
+        public String getBrand() {
+            return Brand;
+        }
+        public void setBrand(String brand) {
+            Brand = brand;
+        }
+        public String getModel() {
+            return model;
+        }
+        public void setModel(String model) {
+            this.model = model;
+        }
+        public int getYear() {
+            return year;
+        }
+        public void setYear(int year) {
+            this.year = year;
+        }
+        public String getFuel_type() {
+            return Fuel_type;
+        }
+        public void setFuel_type(String fuel_type) {
+            Fuel_type = fuel_type;
+        }
+        public String getColor() {
+            return Color;
+        }
+        public void setColor(String color) {
+            Color = color;
+        }
+        public double getMileage_kmpl() {
+            return Mileage_kmpl;
+        }
+        public void setMileage_kmpl(double mileage_kmpl) {
+            Mileage_kmpl = mileage_kmpl;
+        }
+
+
+    }
